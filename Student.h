d15cf903e0e7bf6161e0d83d390613733bbeb627
Ethan Jones <63327961+ethan-jones@users.noreply.github.com>
2021-03-08 21:20:26 -0500

    Update Student.h

--- Student.h
+++ Student.h
@@ -16,35 +16,8 @@ public:
   string getName();
   void setName(string newName);
  
- /* void setName(string newName);
-  bool operator==(const Student& y);
-  bool operator>(const Student& y);
-  bool operator<(const Student& y);
-  */
   friend ostream& operator<<(ostream& os, const Student& s);
   friend istream& operator>>(istream& is, Student& s);
-  
-   bool operator != (const Student &s2)
-{
-return id == s2.id;
-}
-  bool operator == (const Student &s2)
-{
-return id == s2.id;
-}
-//operator < to check if the id of this object is less than s2.id
-bool operator < (const Student &s2)
-{
-return id < s2.id;
-}
-
-//operator > to check if the id of this object is greater than s2.id
-bool operator > (const Student &s2)
-{
-return id > s2.id;
-}
-  
-};
 
 #endif
   
